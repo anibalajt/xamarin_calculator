a0adb1695ea26c84d03adf29859965cb35c95f82
anibalajt <anibalajt@gmail.com>
2021-08-22 20:34:08 +0800

    first commit

---
+++ calculator/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        private string myStringProperty;
+        public string MyStringProperty
+        {
+            get { return myStringProperty; }
+            set
+            {
+                myStringProperty = value;
+                OnPropertyChanged(nameof(MyStringProperty)); // Notify that there was a change on this property
+            }
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+
+            MyStringProperty = "New label text"; // It will be shown at your label
+
+
+        }
+
+    }
+}
